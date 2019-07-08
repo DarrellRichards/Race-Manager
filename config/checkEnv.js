7c2f6204827bc7734bdac03c470e2d1481696260
Darrell Richards <drichards@valcom.com>
2019-07-07 23:23:51 -0400

    Fixed eslint issues

--- config/checkEnv.js
+++ config/checkEnv.js
@@ -1,6 +1,8 @@
-exports.fetchEnv = fetchEnv = (variable) => {
-    if (process.env[variable] === undefined) {
-        throw new Error(`You must provide a value for environmental variable ${variable}`);
-    }
-    return process.env[variable];
+const fetchEnv = (variable) => {
+  if (process.env[variable] === undefined) {
+    throw new Error(`You must provide a value for environmental variable ${variable}`);
+  }
+  return process.env[variable];
 };
+
+module.exports = fetchEnv();
